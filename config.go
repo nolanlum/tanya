09cc26e82b844eea594486d39e1ef7112285ef60
Arlan Jaska <ajaska@berkeley.edu>
2018-03-14 00:28:36 -0700

    Generate a config if one does not exist

--- config.go
+++ config.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/BurntSushi/toml"
+	"github.com/nolanlum/tanya/token"
 )
 
 type slack struct {
@@ -15,15 +18,46 @@ type Config struct {
 	Slack slack
 }
 
-// ParseConfig reads a ./config.toml file and retruns a parsed config
-func ParseConfig() (*Config, error) {
+// parses a config if it exists, or generates a new one
+func LoadConfig() (*Config, error) {
 	tomlData, err := ioutil.ReadFile("config.toml")
+
+	if os.IsNotExist(err) {
+		fmt.Println("config.toml does not exist, generating one...")
+		return initializeConfig()
+	} else if err != nil {
+		return nil, err
+	}
+
+	return parseConfig(string(tomlData))
+}
+
+// initializeConfig interactively generates and writes a config
+func initializeConfig() (*Config, error) {
+	var conf Config
+	token, err := token.GetSlackToken()
 	if err != nil {
 		return nil, err
 	}
 
+	conf.Slack.Token = token
+
+	fmt.Print("Writing config.toml...")
+	f, err := os.Create("config.toml")
+	if err != nil {
+		return nil, err
+	}
+	if err := toml.NewEncoder(f).Encode(conf); err != nil {
+		return nil, err
+	}
+	fmt.Println("Done")
+	return &conf, nil
+}
+
+// parseConfig reads a toml string and returns a parsed config
+func parseConfig(tomlData string) (*Config, error) {
 	var conf Config
-	if _, err = toml.Decode(string(tomlData), &conf); err != nil {
+	if _, err := toml.Decode(string(tomlData), &conf); err != nil {
 		return nil, err
 	}
 
